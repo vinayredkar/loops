e4b0435a5ab5a012085ab2ded15c01bbdff56c8e
vinayredkar <86841694+vinayredkar@users.noreply.github.com>
2021-07-31 11:37:34 +0530

    loop 01 0101 010101
    my captain

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int i,j;
+  printf(" enter an= number);
+  scanf("%d",&n);
+  for (i=1; i<=n;i++)
+  {
+  for(j=0;j<2*i;j++)
+  print("%d",j%2);
+printf(" ");
+}
+return 0;
+}
